b7551a6cfe530644f18687c79593c6885eeb5b2b
rbhatt22 <rutvijbhatt22@gmail.com>
2020-08-12 18:01:07 -0400

    Adding 404 error handling

---
+++ src/main/java/demo/rbhatt/spring/mvc/controller/ErrorController.java
@@ -0,0 +1,25 @@
+package demo.rbhatt.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller("error")
+public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(HttpServletRequest httpServletRequest, Exception ex) {
+
+        ModelAndView mv = new ModelAndView();
+
+        mv.addObject("exception", ex.getLocalizedMessage());
+        mv.addObject("url", httpServletRequest.getRequestURL());
+
+        mv.setViewName("error");
+
+        return mv;
+    }
+
+}
